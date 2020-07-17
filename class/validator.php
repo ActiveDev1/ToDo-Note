556020bda89bc53c35b313f5777248bb0192f934
ActiveDev1 <sarbazmahdi.2014@gmail.com>
2020-07-18 02:36:58 +0430

    Edit minimum of name length

--- class/validator.php
+++ class/validator.php
@@ -2,7 +2,7 @@
 
 class Validator {
     public function name($input) {
-        return strlen(trim($input)) >= 4;
+        return strlen(trim($input)) >= 3;
     }
     public function username($input) {
         return strlen(trim($input)) >= 5;
